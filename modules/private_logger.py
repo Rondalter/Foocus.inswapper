3cfbc73806a898f4447ff157e834618103ae0c78
Rondalter <157960926+Rondalter@users.noreply.github.com>
2024-02-03 20:21:15 +0100

    Add files via upload

--- modules/private_logger.py
+++ modules/private_logger.py
@@ -3,28 +3,24 @@ import args_manager
 import modules.config
 import json
 import urllib.parse
-
 from PIL import Image
 from modules.util import generate_temp_filename
-
-
 log_cache = {}
-
-
 def get_current_html_path():
     date_string, local_temp_filename, only_name = generate_temp_filename(folder=modules.config.path_outputs,
                                                                          extension='png')
     html_name = os.path.join(os.path.dirname(local_temp_filename), 'log.html')
     return html_name
-
-
 def log(img, dic):
     if args_manager.args.disable_image_log:
         return
 
     date_string, local_temp_filename, only_name = generate_temp_filename(folder=modules.config.path_outputs, extension='png')
     os.makedirs(os.path.dirname(local_temp_filename), exist_ok=True)
-    Image.fromarray(img).save(local_temp_filename)
+    if isinstance (img, Image.Image):
+        img.save(local_temp_filename)
+    else:
+        Image.fromarray(img).save(local_temp_filename)
     html_name = os.path.join(os.path.dirname(local_temp_filename), 'log.html')
 
     css_styles = (
@@ -42,7 +38,6 @@ def log(img, dic):
         "button:hover {background-color: grey; color: black;}"
         "</style>"
     )
-
     js = (
         """<script>
         function to_clipboard(txt) { 
@@ -58,7 +53,6 @@ def log(img, dic):
             textArea.style.top = '10px'
             textArea.setAttribute('readonly', 'readonly')
             document.body.appendChild(textArea)
-
             textArea.select()
             document.execCommand('copy')
             document.body.removeChild(textArea)
@@ -67,12 +61,9 @@ def log(img, dic):
         }
         </script>"""
     )
-
     begin_part = f"<html><head><title>Fooocus Log {date_string}</title>{css_styles}</head><body>{js}<p>Fooocus Log {date_string} (private)</p>\n<p>All images are clean, without any hidden data/meta, and safe to share with others.</p><!--fooocus-log-split-->\n\n"
     end_part = f'\n<!--fooocus-log-split--></body></html>'
-
     middle_part = log_cache.get(html_name, "")
-
     if middle_part == "":
         if os.path.exists(html_name):
             existing_split = open(html_name, 'r', encoding='utf-8').read().split('<!--fooocus-log-split-->')
@@ -80,7 +71,6 @@ def log(img, dic):
                 middle_part = existing_split[1]
             else:
                 middle_part = existing_split[0]
-
     div_name = only_name.replace('.', '_')
     item = f"<div id=\"{div_name}\" class=\"image-container\"><hr><table><tr>\n"
     item += f"<td><a href=\"{only_name}\" target=\"_blank\"><img src='{only_name}' onerror=\"this.closest('.image-container').style.display='none';\" loading='lazy'></img></a><div>{only_name}</div></td>"
@@ -89,20 +79,13 @@ def log(img, dic):
         value_txt = str(value).replace('\n', ' </br> ')
         item += f"<tr><td class='key'>{key}</td><td class='value'>{value_txt}</td></tr>\n"
     item += "</table>"
-
     js_txt = urllib.parse.quote(json.dumps({k: v for k, v in dic}, indent=0), safe='')
     item += f"</br><button onclick=\"to_clipboard('{js_txt}')\">Copy to Clipboard</button>"
-
     item += "</td>"
     item += "</tr></table></div>\n\n"
-
     middle_part = item + middle_part
-
     with open(html_name, 'w', encoding='utf-8') as f:
         f.write(begin_part + middle_part + end_part)
-
     print(f'Image generated with private log at: {html_name}')
-
     log_cache[html_name] = middle_part
-
     return
